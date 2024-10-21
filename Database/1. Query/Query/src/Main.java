4e5c825dd22d44563cc9954585fda5ee5b87b979
godwu69 <godwu69@gmail.com>
2024-10-21 11:15:20 +0700

    update

--- Database/1. Query/Query/src/Main.java
+++ Database/1. Query/Query/src/Main.java
@@ -1,16 +1,11 @@
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.CallableStatement;
-import java.sql.SQLException;
-
-// CREATE PROCEDURE first
+import java.sql.*;
 
 // CREATE PROCEDURE addCustomer(
 //    IN Name longtext,
 //    IN Password longtext,
 //    IN Balance decimal(65,30)
-//)
+// )
 // BEGIN
 //    INSERT INTO customers (Name, Password, Balance) VALUES (Name, Password, Balance);
 // END;
@@ -18,18 +13,34 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Connecting to database...");
         String url = "jdbc:mysql://godwu69.mysql.database.azure.com:3306/ATMManagementDb";
         String user = "godwu69";
         String password = "Hoang123@#";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Loading...");
-            CallableStatement stmt = connection.prepareCall("{call addCustomer(?, ?, ?)}");
-            stmt.setString(1, "Dio Brando");
+            System.out.println("Connected to database");
+            System.out.println("Adding Customer...");
+            CallableStatement stmt = connection.prepareCall("{CALL addCustomer(?, ?, ?)}");
+            stmt.setString(1, "This is Name");
             stmt.setString(2, "password");
-            stmt.setBigDecimal(3, new BigDecimal("3000.50"));
+            stmt.setBigDecimal(3, new BigDecimal("3000.5"));
             stmt.execute();
             System.out.println("Customer added successfully.");
+            stmt.close();
+
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM customers");
+            ResultSet rs = pstmt.executeQuery();
+            System.out.println("Customer list:");
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                String pass = rs.getString(3);
+                BigDecimal balance = rs.getBigDecimal(4);
+                System.out.println("ID: " + id + ", Name: " + name + ", Password: " + pass + ", Balance: " + balance);
+            }
+            rs.close();
+            pstmt.close();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
